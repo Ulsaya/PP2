77ac0f80c771f82770aec71774cbb1af8cbd66ee
Ulsaya <ulsaya056@gmail.com>
2019-02-05 15:46:29 +0600

    asdfsdf

---
+++ week1/Task1/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static bool prime(int n)
+        {
+        
+                if (n == 1)
+                    return false;
+                for (int i = 2; i < n; i++)
+                    if (n % i == 0)
+                        return false;
+
+                return true;
+            }
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+            int d = int.Parse(s);
+            string k = Console.ReadLine();
+            string[] arr = k.Split();
+            int cnt = 0;
+            List<int> array = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                int num = int.Parse(arr[i]);
+
+                if (prime(num) == true)
+                {
+                    cnt++;
+                    array.Add(num);
+                }
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(' ');
+            }
+
+
+        }
+         
+        
+    }
+}
---
+++ week1/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{ 
+    class Student
+    {
+        public string name;
+        public string id;
+        public int yearofstudy;
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+            yearofstudy = 0;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getID();
+        {
+            return this.id;
+        }
+
+        public int Increment()
+        {
+           return ++yearofstudy;
+        }
+    }
+}
+
+
+    
+
