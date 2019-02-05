3321a818a342652d21f9c368cee51a96b9bef70a
Ulsaya <ulsaya056@gmail.com>
2019-02-05 16:44:17 +0600

    hbhw

---
+++ week2/task1/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static bool polindrome(string k)
+        {
+            int n = k.Length; //ababa n= 5
+            for (int i=0; i<k.Length; i++) //k[0]!= k[5-0-1]
+            {
+                if (k[i] != k[n -i -1])
+                {
+                    return false;
+                }
+            }
+              return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\ASUS\Desktop\key.txt");
+            string k = sr.ReadToEnd();
+            if (polindrome(k) == true)
+            {
+                Console.WriteLine("Yes");
+            
+            }
+            sr.Close();
+
+        }
+    }
+}
---
+++ week2/task2/task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static bool prime(int d)
+        {
+            for (int i = 2; i < d; i++)
+            {
+                if (d % i == 0)
+                    return false;
+
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\ASUS\Desktop\text.txt");
+            string k = sr.ReadToEnd();
+            string[] arr = k.Split(' ');
+            sr.Close();
+
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\ASUS\Desktop\text1.txt");
+            string res = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int number = int.Parse(arr[i]);
+                if (prime(number))
+                {
+                    res = res + number + ' ';
+                }
+
+            }
+            sw.Write(res);
+            sw.Close();
+        }
+    }
+}
