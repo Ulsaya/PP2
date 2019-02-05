24bd2cbed9191e7e1817109ae1d7587d23772850
Ulsaya <ulsaya056@gmail.com>
2019-02-05 15:54:33 +0600

    xnsk

---
+++ week1/task3/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string s = Console.ReadLine();
+                int n = int.Parse(s);
+                string k = Console.ReadLine();
+                string[] arr = k.Split();
+                int[] numbers = new int[n * 2];
+                int cnt = -1;
+                for (int i = 0; i < n; i++)
+                {
+                    cnt++;
+                    numbers[cnt] = int.Parse(arr[i]);
+                    cnt++;
+                    numbers[cnt] = int.Parse(arr[i]);
+                }
+                foreach (int num in numbers)
+                {
+                    Console.Write(num);
+                    Console.Write(' ');
+                }
+            
+        }
+    }
+}
